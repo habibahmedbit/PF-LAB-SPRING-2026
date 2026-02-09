40a50a969a611245c582859a6518a8913d5f38c4
Lab8 User <lab8.user@khifast.nu.edu.pk>
2026-02-09 11:36:26 +0500

    Initial comit

---
+++ Task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+     int attendence = 0;
+     printf("Enter the attendence of the class: ");
+     if (attendence <= 75){
+          printf("Eligible For Exam");
+     }
+     else {
+          printf("Not Eligible For Exam");
+     }
+}
